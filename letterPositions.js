7437b605ac773714c6cf2f6113aac448f2f74ba3
Paul Bodner <bodner80@gmail.com>
2022-01-12 16:24:14 -0800

    included assertion to letterPositions.js

--- letterPositions.js
+++ letterPositions.js
@@ -14,4 +14,25 @@ const letterPositions = (array) => {
   return result;
 };
 
-console.log(letterPositions("today i went to the store"));
+//takes an input and an expected result and determines if they are the same
+const assertEqual = function(actual, expected) {
+  console.assert(actual === expected, `${actual} !== ${expected}`);
+  if (actual === expected) {
+    console.log(`👍 👍 👍 Assertion passed: ${actual} === ${expected}`);
+  }
+};
+
+
+//checks if arrays are the same returns true or false
+const eqArrays = (array1, array2) => {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i] || array1.length !== array2.length) {
+      return false;
+    }
+  }
+  return true;
+};
+
+let message = "today i went to the store";
+
+assertEqual(eqArrays(letterPositions(message).i, [6]), true);
