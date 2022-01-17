e6466970facab99c84e29e9d82819502341b983e
Paul Bodner <bodner80@gmail.com>
2022-01-17 15:13:19 -0800

    refactored assertEqual and head into test files

--- head.js
+++ head.js
@@ -1,16 +1,18 @@
-const assertEqual = function(actual, expected) {
+//const assertEqual = require('./test/assertEqual.test');  
+/*function(actual, expected) {
   console.assert(actual === expected, `${actual} !== ${expected}`);
   if (actual === expected) {
     console.log(`👍 👍 👍 Assertion passed: ${actual} === ${expected}`);
   }
-};
+};*/
 
 const head = function(array){
   return array[0];
 };
 
+module.exports = head;
 //assertEqual("Lighthouse Labs", "Bootcamp");
 //assertEqual(1, 1);
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+//assertEqual(head([5,6,7]), 5);
+//assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
