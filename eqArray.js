88bcc49d9cc142da40d9f27e8b9fb5d2c83064b3
Paul Bodner <bodner80@gmail.com>
2022-01-17 16:17:20 -0800

    start refactoring using mocha and chai

--- eqArray.js
+++ eqArray.js
@@ -1,5 +1,5 @@
 const eqArrays = (array1, array2) => {
-  console.log('here')
+  //console.log('here')
   for (let i = 0; i < array1.length; i++){
     if (array1[i] !== array2[i] || array1.length !== array2.length){
       return false;
--- test/head.test.js
+++ test/head.test.js
@@ -1,6 +1,17 @@
+const chai = require('chai');
+const assert = chai.assert;
+
 const head = require('../head');
 const assertEqual = require('../assertEqual')
 
+describe("#head", () => {
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+
+});
